15e45b3d3d23e84d69a7cccfce72cfb5715a1708
hashemihossein <48551198+hashemihossein@users.noreply.github.com>
2026-04-03 21:23:40 +0330

    implement execute order command and execute order command handler

---
+++ src/order/application/commands/execute-order.command.ts
@@ -0,0 +1,3 @@
+export class ExecuteOrderCommand {
+  constructor(readonly orderId: string) {}
+}
---
+++ src/order/application/commands/handler/execute-order.command-handler.ts
@@ -0,0 +1,22 @@
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
+import { ExecuteOrderCommand } from '../execute-order.command';
+import { Order } from 'src/order/domain/order';
+import { AggregateRehydrator } from 'src/order/infrastructure/persistence/esdb/rehydrator/aggregate.rehydrator';
+import { OrderExecutedEvent } from 'src/order/domain/events/order/order-executed.event';
+
+@CommandHandler(ExecuteOrderCommand)
+export class ExecuteOrderCommandHandler implements ICommandHandler<ExecuteOrderCommand> {
+  constructor(
+    private readonly rehydrator: AggregateRehydrator,
+    private readonly eventPublisher: EventPublisher,
+  ) {}
+
+  async execute(command: ExecuteOrderCommand): Promise<Order> {
+    const order = await this.rehydrator.rehydrate(command.orderId, Order);
+
+    this.eventPublisher.mergeObjectContext(order);
+    order.commit();
+
+    return order;
+  }
+}
